e63f9c2c48528cb27f604f215ebc7a54db1d067c
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2023-02-13 22:52:49 -0500

    After the refactor, the throws delcarations inside of the main method are no longer needed

--- src/com/tbonegames/Main.java
+++ src/com/tbonegames/Main.java
@@ -1,20 +1,19 @@
 package com.tbonegames;
 
-import java.awt.AWTException;
 
 public class Main {
 
 	
 	MouseHandler mHandler = new MouseHandler();
 	
-	public Main() throws AWTException {
+	public Main() {
 		
 		
 		mHandler.startCircularScreen();
 	}
 	
 	
-	public static void main(String[] args) throws AWTException {
+	public static void main(String[] args) {
 
 		new Main();
 			
